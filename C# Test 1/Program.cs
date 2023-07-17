ba2c0092d8db13ae473b04467dd557cc777429d2
moooogz <markdavedizon@yahoo.com>
2023-07-17 12:42:21 +0800

    first commit

---
+++ C# Test 1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+String fname, lname;
+
+Console.Write("Enter First Name : ");
+ fname = Console.ReadLine();
+Console.Write("Enter Last Name : ");
+ lname = Console.ReadLine();
+Console.WriteLine("Your Name is "+fname + " " + lname);
+
+
+Console.Read();
---
+++ mooogzapp/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p><h1>Welcome SSU Members</h1></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
